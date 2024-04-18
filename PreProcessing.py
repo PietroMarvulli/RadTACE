a03c93b97d37f3c514f3a70e424c5ce694c307b8
PietroMarvulli <p.marvulli@studenti.poliba.it>
2024-04-18 10:02:50 +0200

    + Extract Radiomics Features

--- PreProcessing.py
+++ PreProcessing.py
@@ -1,7 +1,10 @@
 import os
 import pandas as pd
 
-rad_features = pd.read_csv(r"radfeature_TACE_MULTI.csv")
-
+rad_features = pd.read_csv(r"RadFeatures.csv")
+cols_to_drop = [col for col in rad_features.columns if 'diagnostic' in col]
+cols_to_drop.append("Mask")
+rad_features = rad_features.drop(columns=cols_to_drop)
+rad_features["Image"] = rad_features["Image"].str[35:42]
 print(0)
 
--- utils/create_CSV.py
+++ utils/create_CSV.py
@@ -1,17 +1,17 @@
 import os
 import pandas as pd
-main_dir = r'D:\dataset_TACE_NIfTI\HCC-TACE-SEG'
+main_dir = r'D:\dataset_TACE_NIfTI\HCC-TACE-Seg'
 
-# Initialize lists to store paths
+
 image_paths = []
 mask_paths = []
 
-# Traverse through patient folders
 for patient_folder in os.listdir(main_dir):
     patient_dir = os.path.join(main_dir, patient_folder)
+    print(patient_dir)
     if os.path.isdir(patient_dir):
-        # Traverse through acquisition folders
         for acquisition_folder in os.listdir(patient_dir):
+            print("\t"+acquisition_folder)
             acquisition_dir = os.path.join(patient_dir, acquisition_folder)
             if os.path.isdir(acquisition_dir):
                 # Check if Segmentation.nrrd exists
@@ -19,11 +19,11 @@ for patient_folder in os.listdir(main_dir):
                 if os.path.exists(segmentation_path):
                     files = os.listdir(acquisition_dir)
                     if len(files) >= 2:
-                    # Append image paths in the acquisition folder
-                        file = files[-2]  # Penultimate file
+                        file = files[-2]
                         if file.endswith('.nii.gz'):
                             image_paths.append(os.path.join(acquisition_dir, file))
                             mask_paths.append(segmentation_path)
 
 df = pd.DataFrame({'Image': image_paths, 'Mask': mask_paths})
 df.to_csv(r'D:\dataset_TACE_NIfTI\RadiomicsFeatures\image-mask_SINGLE.csv', index=False)
+df.to_csv(r'C:\Users\marvu\Desktop\GitHub\RadTACE\utils\image-mask_SINGLE.csv', index=False)
