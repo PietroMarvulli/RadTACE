25b8d682f7655ada41336e5cd44539375134ff31
PietroMarvulli <p.marvulli@studenti.poliba.it>
2024-04-19 12:08:14 +0200

    + Extract Radiomics Features

--- PreProcessing.py
+++ PreProcessing.py
@@ -1,10 +1,11 @@
 import os
 import pandas as pd
 
-rad_features = pd.read_csv(r"RadFeatures.csv")
+rad_features = pd.read_csv(r"file/RadFeatures_Lesion.csv")
 cols_to_drop = [col for col in rad_features.columns if 'diagnostic' in col]
 cols_to_drop.append("Mask")
 rad_features = rad_features.drop(columns=cols_to_drop)
 rad_features["Image"] = rad_features["Image"].str[35:42]
+clinical_feature = pd.read_excel(r"file/HCC-TACE-Seg_clinical_data-V2.csv")
 print(0)
 
