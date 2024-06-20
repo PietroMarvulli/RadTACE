020621117c3394fd15eca17ac703db3e6ab4cf39
PietroMarvulli <p.marvulli@studenti.poliba.it>
2024-06-20 10:06:37 +0200

    + Selection Correlated Features by AUC

--- main.py
+++ main.py
@@ -9,16 +9,17 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.neural_network import MLPClassifier
 from xgboost import XGBClassifier
 from sklearn.svm import SVC
+import sys
 from sksurv.linear_model import CoxnetSurvivalAnalysis
 import warnings
 from sklearn.model_selection import cross_val_score, StratifiedKFold, train_test_split
 import seaborn as sns
 from sklearn.model_selection import GridSearchCV, KFold
 from sklearn.exceptions import FitFailedWarning, ConvergenceWarning
-from sklearn.pipeline import make_pipeline
 from sklearn.manifold import TSNE
-from sklearn.metrics import roc_curve, auc
+from sklearn.metrics import roc_curve, auc, roc_auc_score
 import matplotlib.pyplot as plt
+from tqdm import tqdm
 
 def PreprocessRadFeatures(radPath,clinicalPath, thrs):
     rad_features = pd.read_csv(radPath)
@@ -32,7 +33,7 @@ def PreprocessRadFeatures(radPath,clinicalPath, thrs):
     clinical_feature['target'] = (clinical_feature['TTP'] > 14).astype(float)
 
     rad_features['target'] = (clinical_feature['TTP'] > 14).astype(float)
-
+    target = rad_features['target']
     mat = rad_features.drop(columns='target').corr(method='pearson')
     cluster_map = False
     if cluster_map:
@@ -44,8 +45,9 @@ def PreprocessRadFeatures(radPath,clinicalPath, thrs):
         plt.show()
     cutoff_corr = thrs
     high_correlation_pairs = []
-    for i in range(len(mat.columns)):
-        print(i)
+    for i in tqdm(range(len(mat.columns)), ascii=True, desc="Processing correlations"):
+        # print(i)
+        # Print feature pair being processed without interfering with tqdm bar
         for j in range(i + 1, len(mat.columns)):
             if abs(mat.iloc[i, j]) > cutoff_corr:
                 v1 = mat.columns[i]
@@ -54,11 +56,32 @@ def PreprocessRadFeatures(radPath,clinicalPath, thrs):
                 high_correlation_pairs.append((v1, v2, correlation_value))
     pair_data = pd.DataFrame(high_correlation_pairs, columns=['feature1','feature2','correlation value'])
     drop_list = []
-    for row in pair_data.iterrows():
-        if random.random() > 0.5:
-            drop_list.append(row[1][0])
+
+    for index, row in tqdm(pair_data.iterrows(), total=pair_data.shape[0], ascii = True, desc=f"Evaluating features "):
+        feature1 = row['feature1']
+        feature2 = row['feature2']
+
+
+        X_train1, X_test1, y_train, y_test = train_test_split(rad_features[feature1], target, test_size=0.3, random_state=seed)
+        X_train2, X_test2, _, _ = train_test_split(rad_features[feature2], target, test_size=0.3, random_state=seed)
+
+        model1 = LogisticRegression(max_iter=10000)
+        model2 = LogisticRegression(max_iter=10000)
+
+        model1.fit(X_train1.values.reshape(-1,1), y_train.values)
+        model2.fit(X_train2.values.reshape(-1,1), y_train.values)
+
+        y_pred1 = model1.predict_proba(X_test1.values.reshape(-1, 1))[:, 1]
+        y_pred2 = model2.predict_proba(X_test2.values.reshape(-1, 1))[:, 1]
+
+        auc1 = roc_auc_score(y_test, y_pred1)
+        auc2 = roc_auc_score(y_test, y_pred2)
+
+        if auc1 > auc2:
+            drop_list.append(feature2)
         else:
-            drop_list.append(row[1][1])
+            drop_list.append(feature1)
+
     drop_list = set(drop_list)
     rad_features = rad_features.drop(columns=drop_list)
     rad_features = rad_features.set_index('Image', drop=True).rename_axis(None)
@@ -145,6 +168,7 @@ def LassoAnalysis(x,y):
     for value, item in coef:
         if value != 0:
             count += 1
+    print(Fore.WHITE + Style.BRIGHT + "LASSO Analysis" + Style.RESET_ALL)
     print(Fore.GREEN + Style.BRIGHT + f"Number of non-zero coefficients: {count}" + Style.RESET_ALL)
     print(Fore.RED + Style.BRIGHT + f"Rad_Score: " + Style.RESET_ALL)
     print(Fore.CYAN + str('%.4f' % lasso.intercept_) + Style.RESET_ALL + ' + ')
@@ -156,87 +180,44 @@ def LassoAnalysis(x,y):
     features.append('intercept')
     coeff.append(lasso.intercept_)
     df = pd.DataFrame({"feature":features,"coeff":coeff})
-    f = x[df['feature'][:-1]]
-    c = df['coeff'][:-1].values
-    rad_score = f * c
-    rad_score['sum'] = rad_score.sum(axis=1)
-    rad_score['score'] = rad_score['sum'] + df['coeff'].values[-1]
-    x['target']=y
-    x['rad_score'] = rad_score['score']
-    return df, x
-def LassoCoxAnalysis(x,y,plot = False):
-    print( Style.BRIGHT + "\033[4m" + "Lasso CoxPH Model Analysis" + Style.RESET_ALL)
-    coxnet_pipe = make_pipeline(CoxnetSurvivalAnalysis(l1_ratio=1, alpha_min_ratio=0.01, max_iter=10000))
-    warnings.simplefilter("ignore", UserWarning)
-    warnings.simplefilter("ignore", FitFailedWarning)
-    coxnet_pipe.fit(x, y)
-    estimated_alphas = coxnet_pipe.named_steps["coxnetsurvivalanalysis"].alphas_
-    cv = KFold(n_splits=10, shuffle=True, random_state=1969)
-    gcv = GridSearchCV(coxnet_pipe,
-        param_grid={"coxnetsurvivalanalysis__alphas": [[v] for v in estimated_alphas]},
-        cv=cv).fit(x, y)
-    cv_results = pd.DataFrame(gcv.cv_results_)
-    alphas = cv_results.param_coxnetsurvivalanalysis__alphas.map(lambda x: x[0])
-    mean = cv_results.mean_test_score
-    std = cv_results.std_test_score
-    if plot:
-        fig, ax = plt.subplots(figsize=(9, 6))
-        ax.plot(alphas, mean)
-        ax.fill_between(alphas, mean - std, mean + std, alpha=0.15)
-        ax.set_xscale("log")
-        ax.set_ylabel("concordance index")
-        ax.set_xlabel("alpha")
-        ax.axvline(gcv.best_params_["coxnetsurvivalanalysis__alphas"][0], c="C1")
-        ax.axhline(0.5, color="grey", linestyle="--")
-        plt.show()
-    best_model = gcv.best_estimator_.named_steps["coxnetsurvivalanalysis"]
-    best_coefs = pd.DataFrame(best_model.coef_, index=x.columns, columns=["coefficient"])
-    non_zero = np.sum(best_coefs.iloc[:, 0] != 0)
-    print(Fore.GREEN+Style.BRIGHT+f"Number of non-zero coefficients: {non_zero}"+Style.RESET_ALL)
-    formula = list()
-    for index,row in best_coefs.iterrows():
-        if row['coefficient'] != 0:
-            el = [row['coefficient'],str(index)]
-            formula.append(tuple(el))
-            print(Fore.CYAN+str('%.4f'%row['coefficient'])+Style.RESET_ALL+' * '+Fore.YELLOW+str(index)+Style.RESET_ALL)
-    if plot:
-        non_zero_coefs = best_coefs.query("coefficient != 0")
-        coef_order = non_zero_coefs.abs().sort_values("coefficient").index
-        _, ax = plt.subplots(figsize=(6, 8))
-        non_zero_coefs.loc[coef_order].plot.barh(ax=ax, legend=False)
-        ax.set_xlabel("coefficient")
-        ax.grid(True)
-        plt.show()
-    features = []
-    coef = []
-    for el in formula:
-        coef.append(el[0])
-        features.append(el[1])
-    formula = pd.DataFrame({'feature':features,'coeff':coef})
-    print('\033[1;30;47m' + '-' * 50 + '\033[0m')
-    return formula
+    return df
 def ElasticNetAnalysis(x,y):
-    print( Style.BRIGHT + "\033[4m" + "Elastic Net Analysis" + Style.RESET_ALL)
-    elastic_net = ElasticNetCV(cv=10, random_state=1969)
-    elastic_net.fit(x, y)
+    alphas = np.linspace(0.0001, 1, 1000)
+    elastic_net = ElasticNetCV(cv=10, random_state=seed, alphas = alphas, max_iter= 100000, verbose=0).fit(x, y)
     optimal_alpha = elastic_net.alpha_
     optimal_l1_ratio = elastic_net.l1_ratio_
     elastic_net_optimal = ElasticNet(alpha=optimal_alpha, l1_ratio=optimal_l1_ratio)
     elastic_net_optimal.fit(x, y)
-    coefficients = pd.DataFrame({'feature':elastic_net_optimal.feature_names_in_,'coefficient': elastic_net_optimal.coef_})
-    non_zero = np.sum(abs(coefficients.iloc[:, 1]) > 0)
-    print(Fore.GREEN + Style.BRIGHT + f"Number of non-zero coefficients: {non_zero}" + Style.RESET_ALL)
+    coef = list(zip(elastic_net_optimal.coef_, x))
     features = []
     coeff = []
-    for index, row in coefficients.iterrows():
-        if row['coefficient'] != 0:
-            print(Fore.CYAN + str('%.4f' % row['coefficient'] + Style.RESET_ALL + ' * ' + Fore.YELLOW + row['feature'] + Style.RESET_ALL))
-            features.append(row['feature'])
-            coeff.append(row['coefficient'])
-    print('\033[1;30;47m' + '-' * 50 + '\033[0m')
+    count = 0
+    for value, item in coef:
+        if value != 0:
+            count += 1
+    print(Fore.WHITE + Style.BRIGHT + "ElasticNet Analysis" + Style.RESET_ALL)
+    print(Fore.GREEN + Style.BRIGHT + f"Number of non-zero coefficients: {count}" + Style.RESET_ALL)
+    print(Fore.RED + Style.BRIGHT + f"Rad_Score: " + Style.RESET_ALL)
+    print(Fore.CYAN + str('%.4f' % elastic_net_optimal.intercept_) + Style.RESET_ALL + ' + ')
+    for value, item in coef:
+        if value != 0:
+            print(Fore.CYAN + str('%.4f' % value) + Style.RESET_ALL + ' * ' + Fore.YELLOW + str(item) + Style.RESET_ALL)
+            features.append(item)
+            coeff.append(value)
+    features.append('intercept')
+    coeff.append(elastic_net_optimal.intercept_)
     df = pd.DataFrame({"feature": features, "coeff": coeff})
     return df
+def RadScore(x,y,features):
 
+    f = x[features['feature'][:-1]]
+    c = features['coeff'][:-1].values
+    rad_score = f * c
+    rad_score['sum'] = rad_score.sum(axis=1)
+    rad_score['score'] = rad_score['sum'] + features['coeff'].values[-1]
+    x['target'] = y
+    x['rad_score'] = rad_score['score']
+    return x
 def Tsne2D(df):
     warnings.simplefilter("ignore", FutureWarning)
     if 'ID' in df.columns:
@@ -271,22 +252,20 @@ def Tsne3D(df):
     ax.set_ylabel('t-SNE component 2')
     ax.set_zlabel('t-SNE component 3')
     plt.show()
-def lr(df):
-    skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=1969)
+def lr(df, y):
+    skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=seed)
     x = df[[name for name in df if 'target' not in name]]
-    y = df['target']
     param_grid = {'C': [0.1, 1, 10], 'solver': ['lbfgs', 'liblinear']}
     grid = GridSearchCV(LogisticRegression(max_iter=10000), param_grid, cv=skf, scoring='accuracy')
     grid.fit(x, y)
     best_model = grid.best_estimator_
     log_reg_accuracy = cross_val_score(best_model, x, y, cv=skf, scoring='accuracy')
     log_reg_auc = cross_val_score(best_model, x, y, cv=skf, scoring='roc_auc')
-    print(f"                        Accuracy    AUC")
+
     print(f"Logistic Regression --> {log_reg_accuracy.mean():.4f}      {log_reg_auc.mean():.4f}")
-def svm(df):
-    skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=1969)
+def svm(df, y):
+    skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=seed)
     x = df[[name for name in df if 'target' not in name]]
-    y = df['target']
     param_grid = {'C': [0.1, 1, 10], 'kernel': ['linear', 'rbf']}
     grid = GridSearchCV(SVC(probability=True), param_grid, cv=skf, scoring='accuracy')
     grid.fit(x, y)
@@ -294,10 +273,9 @@ def svm(df):
     svm_accuracy = cross_val_score(best_model, x, y, cv=skf, scoring='accuracy')
     svm_auc = cross_val_score(best_model, x, y, cv=skf, scoring='roc_auc')
     print(f"SVM                 --> {svm_accuracy.mean():.4f}      {svm_auc.mean():.4f}")
-def rf(df):
-    skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=1969)
+def rf(df, y):
+    skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=seed)
     x = df[[name for name in df if 'target' not in name]]
-    y = df['target']
     param_grid = {'n_estimators': [50, 100, 200], 'max_depth': [None, 10, 20]}
     grid = GridSearchCV(RandomForestClassifier(random_state=1969), param_grid, cv=skf, scoring='accuracy')
     grid.fit(x, y)
@@ -305,11 +283,9 @@ def rf(df):
     rf_accuracy = cross_val_score(best_model, x, y, cv=skf, scoring='accuracy')
     rf_auc = cross_val_score(best_model, x, y, cv=skf, scoring='roc_auc')
     print(f"Random Forest       --> {rf_accuracy.mean():.4f}      {rf_auc.mean():.4f}")
-def xgb(df):
-
-    skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=1969)
+def xgb(df, y):
+    skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=seed)
     x = df[[name for name in df if 'target' not in name]]
-    y = df['target']
     param_grid = {'n_estimators': [50, 100, 200]}
     grid = GridSearchCV(XGBClassifier(use_label_encoder=False, eval_metric='logloss'), param_grid, cv=skf, scoring='accuracy')
     grid.fit(x, y)
@@ -317,10 +293,9 @@ def xgb(df):
     xgb_accuracy = cross_val_score(best_model, x, y, cv=skf, scoring='accuracy')
     xgb_auc = cross_val_score(best_model, x, y, cv=skf, scoring='roc_auc')
     print(f"XGBoost             --> {xgb_accuracy.mean():.4f}      {xgb_auc.mean():.4f}")
-def mlp(df):
-    skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=1969)
+def mlp(df, y):
+    skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=seed)
     x = df[[name for name in df if 'target' not in name]]
-    y = df['target']
     param_grid = {'hidden_layer_sizes': [(32,), (32,16), (64,)]}
     grid = GridSearchCV(MLPClassifier(max_iter=10000, random_state=1969), param_grid, cv=skf, scoring='accuracy')
     grid.fit(x, y)
@@ -336,7 +311,7 @@ if __name__ == '__main__':
 
     thr = 0.85
     thr_s = f"{int(thr * 100):03d}"
-    file = "feature_"+thr_s+".csv"
+    file = "AUC_feature_"+thr_s+".csv"
     clinical_path = "C:\\Users\\marvu\\Desktop\\GitHub\\RadTACE\\file\\clinical_data.csv"
     if file not in os.listdir("C:\\Users\\marvu\\Desktop\\GitHub\\RadTACE\\file\\reducted\\"):
         radiomics_csv = "radiomics_features.csv"
@@ -359,34 +334,84 @@ if __name__ == '__main__':
     ClinicalFeatures = PreprocessClinicalFeatures(clinical_path)
     RadFeatures, ClinicalFeatures = IDMerge(RadFeatures,ClinicalFeatures)
 
+    RadFeatures = RadFeatures.reset_index()
+    RadFeatures = RadFeatures.drop( columns = 'index')
+    ClinicalFeatures = ClinicalFeatures.reset_index()
+    ClinicalFeatures = ClinicalFeatures.drop( columns = 'index')
     # Divided the dataset in train and test
-
+    clinic = ClinicalFeatures['hepatitis']
     target = RadFeatures['target'].values
     features = RadFeatures.drop(columns=['ID','target'])
-    x_train,x_test,y_train,y_test = train_test_split(features,target,test_size=0.2,random_state=seed, )
+    lasso1 = LassoAnalysis(features,target)
+    en1 = ElasticNetAnalysis(features,target)
+    data = RadScore(features,target,lasso1)
+    x_train,x_test,y_train,y_test = train_test_split(features,target,test_size=0.2,random_state=seed)
 
 
-    # Computing LASSO methods and Calculate Radiomics Score with 10f cv
+    ## Computing LASSO methods and Calculate Radiomics Score with 10f cv
     # and calculate a Rad_Score for every patient
-    lasso, data_score = LassoAnalysis(x_train, y_train)
+    # lasso2 = LassoAnalysis(x_train, y_train)
     # Find the best threshold
-    data = data_score[['target','rad_score']]
-    data = data.sort_values(by = 'rad_score')
-    fpr, tpr, thresholds = roc_curve(data['target'],data['rad_score'])
+    data_score=RadScore(x_train,y_train,lasso1)
+    data_train = data_score[['target','rad_score']]
+    data_train_sort = data_train.sort_values(by = 'rad_score')
+    fpr, tpr, thresholds = roc_curve(data_train_sort['target'],data_train_sort['rad_score'])
     youden_index = tpr + (1-fpr) -1
     optimal_threshold = thresholds[np.argmax(youden_index)]
-    # Plot ROC Curve
-    # roc_auc = auc(fpr, tpr)
-    # plt.figure()
-    # plt.plot(fpr, tpr, color='darkorange', lw=2, label=f'Curva ROC (area = {roc_auc:.2f})')
-    # plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
-    # plt.xlim([0.0, 1.0])
-    # plt.ylim([0.0, 1.10])
-    # plt.xlabel('Tasso di falsi positivi (False Positive Rate)')
-    # plt.ylabel('Tasso di veri positivi (True Positive Rate)')
-    # plt.title('Curva ROC')
-    # plt.legend(loc="lower right")
-    # plt.show()
-    y_pred = (data['rad_score'] >= optimal_threshold).astype(int)
-    accuracy = np.mean(y_pred == data['target'])
-    print(0)
+    y_pred = (data_train['rad_score'] >= optimal_threshold).astype(int)
+    accuracy_train = np.mean(y_pred == data_train['target'])
+    auc_train = auc(fpr, tpr)
+
+    data_score_test = RadScore(x_test,y_test,lasso1)
+    data_test = data_score_test[['target','rad_score']]
+    data_test_sort = data_test.sort_values(by = 'rad_score')
+    fpr, tpr, thresholds = roc_curve(data_test_sort['target'], data_test_sort['rad_score'])
+    youden_index = tpr + (1 - fpr) - 1
+    optimal_threshold = thresholds[np.argmax(youden_index)]
+    y_pred = (data_test['rad_score'] >= optimal_threshold).astype(int)
+    accuracy_test = np.mean(y_pred == data_test['target'])
+    auc_test = auc(fpr, tpr)
+
+
+    # Clinical variable retained after UA and MA is only 'hepatitis'
+    c_train,c_test,cy_train,cy_test = train_test_split(clinic,target,test_size=0.2,random_state=seed)
+    clin_radscore = pd.DataFrame({"hepatitis":clinic.values,"rad_score":data['rad_score'].values})
+    clin_radscore_train = pd.DataFrame({"hepatitis":c_train.values,"rad_score":data_train['rad_score'].values})
+    clin_radscore_test = pd.DataFrame({"hepatitis":c_test.values,"rad_score":data_test['rad_score'].values})
+
+    # Radiomcs varibale foundend in LASSO analysis
+    radiomics_feature = features[lasso1['feature'][:-1]]
+    radiomics_feature_train = x_train[lasso1['feature'][:-1]]
+    radiomics_feature_test = x_test[lasso1['feature'][:-1]]
+
+    #Combined Radiomics + Clinical features
+    combined = radiomics_feature.copy()
+    combined['hepatitis'] = clinic.values
+    combined_train = radiomics_feature_train.copy()
+    combined_train['hepatitis'] = c_train
+    combined_test = radiomics_feature_test.copy()
+    combined_test['hepatitis'] = c_test
+
+    #train and test models
+    print("                        Accuracy    AUC")
+    print(Fore.LIGHTWHITE_EX + Style.BRIGHT + "Clinic + Rad_Score" + Style.RESET_ALL)
+    lr(clin_radscore,target)
+    svm(clin_radscore,target)
+    rf(clin_radscore,target)
+    xgb(clin_radscore,target)
+    mlp(clin_radscore,target)
+    print(Fore.LIGHTWHITE_EX + Style.BRIGHT + "Radiomics Features" + Style.RESET_ALL)
+    lr(radiomics_feature,target)
+    svm(radiomics_feature,target)
+    rf(radiomics_feature,target)
+    xgb(radiomics_feature,target)
+    mlp(radiomics_feature,target)
+    print(Fore.LIGHTWHITE_EX + Style.BRIGHT + "Clinic + Radiomics Features" + Style.RESET_ALL)
+    lr(combined,target)
+    svm(combined,target)
+    rf(combined,target)
+    xgb(combined,target)
+    mlp(combined,target)
+
+    a = 0
+
